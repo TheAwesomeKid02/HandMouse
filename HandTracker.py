052dc34afd2f2875605d6787cf0bfb7c3d1251d5
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2024-08-12 09:38:29 -0400

    Added more documentation

--- HandTracker.py
+++ HandTracker.py
@@ -15,6 +15,7 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
+#USED TO CALCULATE FRAMES PER SECOND
 prevTime = 0
 currTime = 0
 
@@ -29,6 +30,9 @@ pag.moveTo(def_x,def_y,0.5)
 x_change = 600
 y_change = 335
 
+
+#These are the x and y coordinates of the index finger, thumb, and middle finger
+#They will be modified in the program shortly
 x_index = 0
 y_index = 0
 
@@ -39,6 +43,7 @@ x_middle = 0
 y_middle = 0
 
 while True:
+    #Everytime the camera window is closed, it reopens due to this while loop
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
@@ -51,12 +56,11 @@ while True:
                 #print(id, cx, cy)
 
 
-                if id == 8:
+                if id == 8: #8 is the id for the index finger
                     x_index, y_index = cx, cy
-                if id == 4:
+                if id == 4: #4 is the id for the thumb
                     x_thumb, y_thumb = cx, cy
-
-                if id == 12:
+                if id == 12: #12 is the id for the middle finger
                     x_middle, y_middle = cx, cy
 
                 if id == 8 or id == 4 or id == 12:
@@ -95,7 +99,6 @@ while True:
     prevTime = currTime
 
     cv2.putText(img, f'FPS: {str(int(fps))}', (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 3)
-
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
